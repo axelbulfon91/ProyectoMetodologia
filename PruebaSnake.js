b1615f0230c8ae48770dddd5305940de0e5b8e30
facundogarcia95 <48775833+facundogarcia95@users.noreply.github.com>
2019-05-08 15:05:01 -0300

    Add files via upload
    Inclusión de la clase Teclado para control manual de la Serpiente (Snake).

---
+++ PruebaSnake.js
@@ -0,0 +1,41 @@
+var escala = 20;
+var s, c,k; // objetos serpiente, comida y teclado.
+var columnas, filas, tamañoAreaDeJuego = 400; 
+var arriba = false, abajo = false, der = false, izq = false;
+var teclaW = false, teclaS = false, teclaD = false, teclaA = false;
+
+	
+	function setup() {
+	  	
+	  	frameRate(10);  	
+	  	createCanvas(tamañoAreaDeJuego, tamañoAreaDeJuego + 50);
+	  	columnas = floor(tamañoAreaDeJuego/escala);
+	  	filas = floor(tamañoAreaDeJuego/escala);
+	  	s = new Serpiente();  	// crea objeto serpiente
+	  	c = new Comida();  		// crea objeto comida
+		k = new Teclado();
+	  	c.posicionarComida(columnas,filas); // posiciona la primera comida
+	  	k.Teclado();
+	}
+
+
+	function draw() {
+	  
+		background(50, 200, 100);
+		fill(50,200,150)	
+		rect(0,400,400,400);
+		fill (0);
+		textSize(16);
+		text('Aca van los datos del proceso y etc', 20, 430);
+		
+		
+		s.muere();				// en cada frame comprueba si la serpiente muere
+		s.actualizarPos();		// en cada frame la serpiente actualiza su posicion
+	  	s.mostrar();			// en cada frame se dibuja la serpiente (con su nueva posicion generada arriba)
+		s.comprobarSiCome(); 	// en cada frame comprueba si la serpiente come, de ser asi vuelve a crear otra comida a travez del metodo posicionarComida
+		c.dibujarComida();		// dibuja comida
+		k.Teclado();            // llama funcion teclado.
+
+	}
+
+
---
+++ Teclado.js
@@ -0,0 +1,67 @@
+
+function Teclado(){
+
+	if(event.keyCode === 38 && arriba === false && abajo === false){
+
+		s.direccion(0, -1);
+		arriba = true;
+		abajo = true;
+		izq = false;
+		der = false;
+
+	}else if (event.keyCode === 40 && abajo === false && arriba === false){
+
+		s.direccion(0, 1);
+		abajo = true;
+		arriba = true;
+		izq = false;
+		der = false;
+
+	}else if (event.keyCode === 37 && izq === false && der === false){
+
+		s.direccion(-1, 0);
+		izq = true;
+		abajo = false;
+		arriba = false;
+		der = true;
+
+	}else if (event.keyCode === 39 && der === false && izq === false){
+
+		s.direccion(1, 0);
+		der = true;
+		abajo = false;
+		izq = true;
+		arriba = false;
+	}else if(event.keyCode === 87 && teclaW === false && teclaS === false){
+
+		s.direccion(0, -1);
+		arriba = true;
+		abajo = true;
+		izq = false;
+		der = false;
+
+	}else if (event.keyCode === 83 && teclaS === false && teclaW === false){
+
+		s.direccion(0, 1);
+		abajo = true;
+		arriba = true;
+		izq = false;
+		der = false;
+
+	}else if (event.keyCode === 65 && teclaA === false && teclaD === false){
+
+		s.direccion(-1, 0);
+		izq = true;
+		abajo = false;
+		arriba = false;
+		der = true;
+
+	}else if (event.keyCode === 68 && teclaD === false && teclaA === false){
+
+		s.direccion(1, 0);
+		der = true;
+		abajo = false;
+		izq = true;
+		arriba = false;
+	}
+}
