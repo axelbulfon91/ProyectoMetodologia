afe8567e99f2a887f3a49178fc74da0fa50f9e57
Axel Bulfon <43324571+axelbulfon91@users.noreply.github.com>
2019-05-24 19:56:27 -0300

    Actualizacion clase Serpiente
    Objeto serpiente ahora obtiene funcionalidad de "ver" en una distancia seteable por el usuario, en busca de comida, paredes y su propia cola, (funcion vista)

--- Serpiente.js
+++ Serpiente.js
@@ -3,19 +3,24 @@ var com;
 function Serpiente () {
 
 	this.x = 0;
-	this.y = 0;
+	this.y = 0;			
 	this.xVel = 1;
-	this.yVel = 0;
+	this.yVel = 0;	
 	this.tam = 0;
 	this.cola = [];
+	this.punt = 0;
+	this.rangoVision = escala // El rango de vision inicial es de una vez el tamaño de la cabeza, osea el cuadro delante de la misma
+	this.maximaVision = this.rangoVision * 5  // Alcance maximo de la vision (5 veces el tamaño de la cabeza)
 	com = new Comida();
+	
 	this.tamañoAreaDeJuego = 400;
 
 	this.direccion = function(x, y){
 
 		this.xVel = x;
 		this.yVel = y;
-
+		
+		
 	}
 	
 	this.muere = function(){
@@ -27,8 +32,12 @@ function Serpiente () {
 
 			if (distanciaAMorir < 1) {
 
-				this.tam = 0;
-				this.cola = [];
+				//this.tam = 0;
+				//this.cola = [];
+				this.devolverPuntaje();
+				textAlign(CENTER);
+				text('Juego Terminado!!! presione F5 para recargar pagina', 200, 200);
+				frameRate(0);
 			}
 		}
 
@@ -49,12 +58,13 @@ function Serpiente () {
 		}
 
 	}
+	
 	this.actualizarPos = function(){
 			
 		for(var i = 0; i < this.cola.length - 1 ; i++){
 
 			this.cola[i] = this.cola[i+1];
-
+			
 		}			
 	
 		this.cola[this.tam - 1] = createVector(this.x, this.y);
@@ -64,7 +74,7 @@ function Serpiente () {
 
 		this.x = constrain(this.x, 0, tamañoAreaDeJuego - escala);
 		this.y = constrain(this.y, 0, tamañoAreaDeJuego - escala);
-
+		
 
 	}
 
@@ -90,7 +100,86 @@ function Serpiente () {
  			com.posicionarComida(columnas, filas);
  			print ("La serpiente comio");
   		}
+		
+	}	
+	
+	this.devolverPuntaje = function(){
+		
+		this.punt = this.tam * 5;
+		
+	}
 
+	this.vista = function(){
+
+	do{
+		
+		var distanciaDeVision = createVector(this.x , this.y);
+
+		// -------------------------------------------------------------------------------------------------------Va mirando comida
+		if(distanciaDeVision.x === posComida.x && distanciaDeVision.y - this.rangoVision === posComida.y){
+
+			print ('--------------Esta viendo COMIDA arriba --------------');
+
+		}else if(distanciaDeVision.x === posComida.x && distanciaDeVision.y + this.rangoVision === posComida.y){
+
+			print ('--------------Esta viendo COMIDA abajo --------------');	
+
+		}else if(distanciaDeVision.x + this.rangoVision === posComida.x && distanciaDeVision.y === posComida.y){
+
+			print ('--------------Esta viendo COMIDA a la derecha --------------');	
+
+		}
+		else if(distanciaDeVision.x - this.rangoVision === posComida.x && distanciaDeVision.y === posComida.y){
+
+			print ('--------------Esta viendo COMIDA a la izquierda --------------');	
+
+		}
+		// -------------------------------------------------------------------------------------------------------Va mirando paredes
+		if( distanciaDeVision.y - this.rangoVision === 0){
+
+			print ('--------------Esta viendo PARED arriba --------------');
+
+		}else if(distanciaDeVision.y + this.rangoVision === 400){
+
+			print ('--------------Esta viendo PARED abajo --------------');	
+
+		}else if(distanciaDeVision.x + this.rangoVision === 400){
+
+			print ('--------------Esta viendo PARED a la derecha --------------');	
+
+		}else if(distanciaDeVision.x - this.rangoVision === 0){
+
+			print ('--------------Esta viendo PARED a la izquierda --------------');	
+
+		}
+		
+		// -------------------------------------------------------------------------------------------------------Va mirando su cola
+		
+		for(var i = 0; i < this.cola.length; i++){
+
+			if(distanciaDeVision.x === this.cola[i].x && distanciaDeVision.y - this.rangoVision === this.cola[i].y){
+
+				print ('--------------Esta viendo SU COLA arriba --------------');
+
+			}else if(distanciaDeVision.x === this.cola[i].x && distanciaDeVision.y + this.rangoVision === this.cola[i].y){
+
+				print ('--------------Esta viendo SU COLA abajo --------------');	
+
+			}else if(distanciaDeVision.x + this.rangoVision === this.cola[i].x && distanciaDeVision.y === this.cola[i].y){
+
+				print ('--------------Esta viendo SU COLA a la derecha --------------');	
+
+			}else if(distanciaDeVision.x - this.rangoVision === this.cola[i].x && distanciaDeVision.y === this.cola[i].y){
+
+				print ('--------------Esta viendo SU COLA a la izquierda --------------');	
+
+			}
+		}
+		this.rangoVision += escala;
+
+	}while(this.rangoVision <= this.maximaVision);		
+	
+	this.rangoVision = escala;	
 	}
 
 }
