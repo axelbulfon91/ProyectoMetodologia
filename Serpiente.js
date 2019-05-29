44f4c1c590ec26231ea8f503e7780d0b13ea65f7
ANDY <andres.pereyra@alumnos.frm.utn.edu.ar>
2019-05-29 12:26:51 -0300

    Agregado y Correccion de botones, Agregado de Sonidos, Correccion de errores

--- Serpiente.js
+++ Serpiente.js
@@ -1,5 +1,6 @@
 var com;
 
+
 function Serpiente () {
 
 	this.x = 0;
@@ -19,51 +20,57 @@ function Serpiente () {
 
 	}
 	
+
+
 	this.muere = function(){
 
 		for (var i = 0; i < this.cola.length ; i++) {
-		
 			var pos = this.cola[i];
 			var distanciaAMorir = dist (this.x, this.y, pos.x, pos.y);
 
 			if (distanciaAMorir < 1) {
-
 				this.tam = 0;
 				this.cola = [];
-				this.devolverPuntaje(); 
-				
-				alert("GAME OVER");
-            document.location.reload();
-            clearInterval(interval); // Needed for Chrome to end game
+				this.devolverPuntaje();
+				sonMov.remove(); 
+				pantallaFin = true;
+
+				 if(estadoSonido === true){
+          			sonMuert.play();
+         		 }else{
+          			sonMuert.remove();
+          }	
 			}
 		}
-
 	}
 	
+
+
 	this.comer = function(pos){
 
 		var distanciaAComida = dist(this.x, this.y, pos.x, pos.y);
 
 		if (distanciaAComida < 1) {
-
+   		 if(estadoSonido === true){
+          	sonFrut.play();
+         }else{
+            sonFrut.remove();
+          }
 			this.tam++;
 			return true;
-
 		}else{
-
 			return false;
 		}
-
 	}
 	
+
+
 	this.actualizarPos = function(){
-			
-		for(var i = 0; i < this.cola.length - 1 ; i++){
 
+		for(var i = 0; i < this.cola.length - 1 ; i++){	
 			this.cola[i] = this.cola[i+1];
+		}	
 
-		}			
-	
 		this.cola[this.tam - 1] = createVector(this.x, this.y);
 
 		this.x = this.x + this.xVel * escala;
@@ -72,44 +79,41 @@ function Serpiente () {
 		this.x = constrain(this.x, 0, tamañoAreaDeJuego - escala);
 		this.y = constrain(this.y, 0, tamañoAreaDeJuego - escala);
 
-
 	}
+
+
+
 
 	this.mostrar = function(){
 
 		fill(255);
+		if( estadoSonido === true){
+			sonMov.play();
+		}else{
+			sonMov.remove();
+		}	
 
 		for(var i = 0; i < this.tam; i++){
-
-			rect(this.cola[i].x , this.cola[i].y, escala, escala);
-
+			rect(this.cola[i].x , this.cola[i].y, escala, escala);		
 		}
-
 		rect(this.x, this.y, escala, escala);
-
-		
 	}
+
+
+
 
 	this.comprobarSiCome = function(){
 
 		if(this.comer(com.devolverPosicionComida())){
-
  			com.posicionarComida(columnas, filas);
  			print ("La serpiente comio");
   		}
-		
 	}	
 	
-	this.devolverPuntaje = function(){
-		
-		this.punt = this.tam * 5;
-		
-	} 
-	this.PantallaInicial = function(){
-	
-		background(50, 200, 100);
-		fill(50,200,150)	
-		
-	}
 
+
+	this.devolverPuntaje = function(){
+		this.punt = this.tam * 5;	
+	} 
+	
 }
